c6d98800f61c4140f5084da04a35bb4f844e0792
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-02-05 22:08:39 +0530

    modified

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,6 @@ class TodoList(models.Model):
 	name=models.CharField(max_length=250)
 	priority=models.CharField(max_length=250)
 	date=models.DateTimeField()
+
+	def __str__(self):
+		return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import TodoList
 # Create your views here.
 def add_task(request):
 	if request.method=='POST':
 		name=request.POST.get('name')
 		priority=request.POST.get('priority')
 		date=request.POST.get('date')
-		redirect('/')
+		task=TodoList(name=name,priority=priority,date=date)
+		task.save()
+		show_task=TodoList.objects.all()
+		return render(request,'add.html',{'task':show_task})
 	else:	
-		return render(request,'add.html')
+		task=TodoList.objects.all()
+		return render(request,'add.html',{'task':task})
+	
