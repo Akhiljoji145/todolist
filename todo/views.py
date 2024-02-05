befbfecbedbf7aa371ff4c98377655346c808bab
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-02-05 11:02:32 +0530

    modified

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 # Create your views here.
+def add_task(request):
+	if request.method=='POST':
+		name=request.POST.get('name')
+		priority=request.POST.get('priority')
+		date=request.POST.get('date')
+		redirect('/')
+	else:	
+		return render(request,'add.html')
